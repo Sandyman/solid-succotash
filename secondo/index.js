db6e5501710af3384c9e47670b83ff4c4e6846c6
Sander Huijsen <sander.huijsen@gmail.com>
2017-07-13 19:51:51 +0800

    Some clarifying comments were added.

--- secondo/index.js
+++ secondo/index.js
@@ -1,6 +1,12 @@
 'use strict';
 
+/**
+ * Function that basically sends back what it got
+ * @param event
+ * @param context
+ * @param callback
+ */
 module.exports.handler = (event, context, callback) => {
-
+  // The body is only a callback
   callback(null, { message: `You said: '${event.msg}'.` });
 };
